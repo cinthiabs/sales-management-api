68da09845327c35e79727c69c86fa896fbea5fc6
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-11-08 20:10:35 -0300

    add class ProductCost

---
+++ src/Domain/Dtos/ProductCostDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Dtos
+{
+    public class ProductCostDto
+    {
+        public int Id { get; set; }
+        public int? IdCost { get; set; }
+        [Required]
+        public decimal TotalProductPrice { get; set; }
+        [Required]
+        public int TotalQuantity { get; set; }
+        [Required]
+        public int QuantityRequired { get; set; }
+        [Required]
+        public decimal IngredientCost { get; set; }
+    }
+    public class ProductTotalCostDto
+    {
+        public int IdProduct { get; set; }
+        [Required]
+        public decimal TotalProductCost { get; set; }
+        public  List<ProductCostDto> UnitCost { get; set; }
+    }
+}
---
+++ src/Domain/Entities/ProductCost.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class ProductCost
+    {
+        public int Id { get; set; }
+        public int IdProductTotalCost { get; set; }
+        public int? IdProduct { get; set; }
+        public int? IdCost { get; set; }
+        public decimal TotalProductPrice { get; set; }
+        public int TotalQuantity { get; set; }
+        public int QuantityRequired { get; set; }
+        public decimal IngredientCost { get; set; }
+        public DateTime DateCreate { get; set; } = DateTime.Now;
+        public DateTime? DateEdit { get; set; }
+    }
+    public class ProductTotalCostDto
+    {
+        public int Id { get; set; }
+        public decimal ProductTotalCost { get; set; }
+        public bool Active { get; set; }
+        public DateTime DateCreate { get; set; } = DateTime.Now;
+        public DateTime? DateEdit { get; set; }
+    }
+}
